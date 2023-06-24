870358b4baea1b0e9a5b3a9b508171c8c455f41e
Astha1691 <131736237+Astha1691@users.noreply.github.com>
2023-06-25 01:13:44 +0530

    Add files via upload
    Added Java 8 Streams

---
+++ ArrayNumberExamples.java
@@ -0,0 +1,172 @@
+package DataStructures.Arrays;
+
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ArrayNumberExamples {
+
+    public static void main(String[] args){
+
+       List<Integer> list = Arrays.asList(1,7,8,9,5,2,36,4,78,222,24,9);
+
+        // Exercise 1 : Given a list of numbers, return the sum of all numbers.
+        sumOfAllNum(list);
+        sumOfMapValues(); ///IMP
+        createMapFromList(); //Create a map from list - VERY IMP
+
+        // Exercise 2 : Given a list of numbers, return the average of all numbers
+        averageOfAllNumbers(list);
+
+        // Exercise 3 : Given a list of numbers, square them and filter the numbers which are greater 100 and then find the average of them
+        getNumbersSquareAndFilterAndAverage(list);
+
+        // Exercise 4 : Given a list of numbers, return the even and odd numbers separately
+        getNumbersEvenAndOddSeperately(list);
+
+        // Exercise 5 : Given a list of numbers, find out all the numbers starting with 2
+        getNumbersStartingWith2(list);
+
+        // Exercise 6 : Given a list of numbers, print the duplicate numbers
+        getDuplicateNumbers(list);
+
+        // Exercise 7 : Given a list of numbers, print the maximum and minimum values
+        getMaxAndMinValueInList(list);
+
+
+    }
+
+    private static void getMaxAndMinValueInList(List<Integer> list) {
+        int max = list.stream().max(Comparator.comparing(i->Integer.valueOf(i))).get();
+        int min = list.stream().min(Comparator.comparing(i->Integer.valueOf(i))).get();
+
+        System.out.println("max: "+max+"min: "+min);
+
+
+    }
+
+    private static void createMapFromList() {
+        //Create a map from list - VERY IMP
+        List<Integer> list1 = Arrays.asList(1,7,8,9,5,2,36,4,78,222,24);
+        Map<Object,Object> m1 = list1.stream().collect(Collectors.toMap(value->value, value->value.toString()));
+        System.out.println("map: "+m1);
+
+    }
+
+    private static void getDuplicateNumbers(List<Integer> list) {
+
+        //method 1 - Collections.frequency(list, num)>1
+        Set<Integer> duplicateNum = list.stream().filter(num->Collections.frequency(list, num)>1).collect(Collectors.toSet());
+        System.out.println("duplicateNum: "+duplicateNum);
+
+        //Method 2 : IMP
+       Set<Integer> duplicateNum1 = list.stream()
+                .collect(Collectors.groupingBy(Function.identity()
+                        , Collectors.counting()))    // create a map {1=1, 2=1, 3=2, 4=2, 5=1, 7=1, 9=2}
+                .entrySet().stream()                 // Map -> Stream
+                .filter(m -> m.getValue() > 1)       // if map value > 1, duplicate element
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toSet());
+
+        System.out.println("duplicateNum1: "+duplicateNum1);
+
+        //Method 3
+        Set<Integer> items = new HashSet<>();
+        Set<Integer> duplicateNum2 = list.stream()
+                .filter(n -> !items.add(n)) // Set.add() returns false if the element was already in the set.
+                .collect(Collectors.toSet());
+
+        System.out.println("duplicateNum2: "+duplicateNum2);
+    }
+
+    private static void getNumbersStartingWith2(List<Integer> list) {
+        List<Integer> startWithTwo =  list.stream().filter(i->i.toString().startsWith("2")).collect(Collectors.toList());
+        System.out.println("startWithTwo: "+startWithTwo);
+
+        //Method 2
+        List<Integer> startWithTwo1 = list.stream().map(num->String.valueOf(num)).filter(num->num.startsWith("2")).map(i->Integer.valueOf(i)).collect(Collectors.toList());
+        System.out.println("startWithTwo1: "+startWithTwo1);
+    }
+
+    private static void getNumbersEvenAndOddSeperately(List<Integer> list) {
+        List<Integer> even =  list.stream().filter(i->i%2 ==0).collect(Collectors.toList());
+        List<Integer> odd =  list.stream().filter(i->i%2 !=0).collect(Collectors.toList());
+
+        System.out.println("even: "+even+"odd: "+odd);
+    }
+
+    private static void getNumbersSquareAndFilterAndAverage(List<Integer> list) {
+
+        OptionalDouble result = list.stream().map(i->i*i)
+                .filter(num->num>100).mapToDouble(d->d).average();
+        System.out.println("result: "+ result);
+    }
+
+    private static void averageOfAllNumbers(List<Integer> list) {
+        //Collectors.averagingInt(d->d)
+        Double avg = list.stream().collect(Collectors.averagingInt(Integer::intValue));
+        Double avg4 = list.stream().collect(Collectors.averagingInt(d->d));
+        System.out.println("avg: "+ avg);
+
+        //mapToDouble(Integer::intValue).average()
+        //mapToDouble(d->d).average();
+        OptionalDouble avg1 = list.stream().mapToDouble(Integer::intValue).average();
+        OptionalDouble avg2 = list.stream().mapToDouble(d->d).average();
+        System.out.println("avg1: "+ avg1 + "avg2: "+ avg2);
+
+
+
+
+    }
+
+    private static void sumOfMapValues() {
+
+        // sum of values of a Map<Object, Integer> data structure
+        Map<Object, Integer> map = new HashMap<>();
+        map.put("Delhi",1);
+        map.put("Bangalore", 2);
+
+        Integer sum5 = map.values()
+                .stream()
+                .mapToInt(Integer::valueOf)
+                .sum();
+        System.out.println("Sum of values of Map : "+ sum5);
+
+        // Incomplete - sum of list of values for each key in map
+        Map<String, List<Integer>> map2 = new HashMap<>();
+        List<Integer> l1 = Arrays.asList(1,7,8,9,5);
+        List<Integer> l2 = Arrays.asList(3,6,8,0,2);
+        map2.put("Delhi", l1);
+        map2.put("Bangalore", l2);
+        // map2.values().stream().mapToInt()
+       // map2.entrySet().stream().mapToInt(Map.Entry::getValue).sum();
+       // System.out.println();
+
+
+
+
+    }
+
+    private static void sumOfAllNum(List<Integer> list) {
+
+        // use summingInt(Integer::valueOf)
+        Integer sum1 = list.stream().collect(Collectors.summingInt(Integer::valueOf));
+        System.out.println("sum1 = "+sum1);
+
+        //use reduce((a,b)->a+b) or reduce(0, Integer::sum)
+        Optional<Integer> sum2 = list.stream().reduce((a, b)-> a+b);
+        Integer sum3 = list.stream().reduce(0, Integer::sum);
+
+        //use IntStream.sum() --> mapToInt()
+        // use mapToInt(Integer::valueOf) or mapToInt(Integer::intValue)
+        Integer sum4 = list.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+
+
+
+    }
+
+
+}
---
+++ ArraysDemo.java
@@ -0,0 +1,104 @@
+
+package DataStructures.Arrays;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class ArraysDemo {
+
+
+    public  static void main(String[] args){
+
+
+        //Create an Int Array : below 3 ways
+        //Method1
+        int arr1[] = new int[]{10,2,7,9,1};
+
+        //Method 2
+        int[] arr2 = {1,2,3,4,5};
+
+        //Method 3
+        int arr[] = new int[5];
+        arr[0]=1;
+        arr[1]=(2);
+        arr[2]=(3);
+        arr[3]=(4);
+        arr[4]=(5);
+
+
+
+        // Array to List
+        arrayToList(arr);
+
+
+        //sort
+        java.util.Arrays.sort(arr1);
+        //System.out.println("Sorted List " + java.util.Arrays.toString(java.util.Arrays.asList(arr1).get(0)));
+        //reverse an array - java 8
+        java.util.Arrays.stream(arr1).boxed()
+                .sorted(Collections.reverseOrder())
+                .mapToInt(Integer::intValue)
+                .toArray();
+        // without streams
+        //Collections class works with wrapper classes only (not with primitive data type -int)
+        //convert int --> Integer
+        Integer[] reverseList = new Integer[arr1.length];
+        int k=0;
+        for(int i : arr1){
+            reverseList[k++] = Integer.valueOf(i);
+        }
+        java.util.Arrays.sort(reverseList, Collections.reverseOrder());
+      //  System.out.println("Reversed Sorted List " + java.util.Arrays.toString(reverseList));
+
+
+        //java 8
+        // arr[int] --> Integer(arr) ---> filter even Integers ---> map(divide each element by 2) ---> collect as List
+        List<Integer> list1 = java.util.Arrays.stream(arr).boxed().filter(p -> p %2 ==0 ).map(i -> i/2 ).collect(Collectors.toList());
+         //System.out.println("Java 8 Convert To List " +list1);
+
+
+        //binary search
+        int result = java.util.Arrays.binarySearch(arr, 10);    // returns index or (-(insertion point)-1)
+        //System.out.println("Binary Search () " +result);
+
+        int arr4[] = new int[]{10,2,7,9,1,10,3,4,7,2,4};
+       // List<Integer> list2 = java.util.Arrays.stream(arr4).boxed().sorted().map(i->i*2).collect(Collectors.toList());
+        List<Integer> list2 = java.util.Arrays.stream(arr4).boxed().map(i->i*2).collect(Collectors.toList());
+        //list2.stream().collect(Collectors::groupingBy);
+
+       // System.out.println("Java 8 Convert To List " +list2);
+
+
+
+
+
+
+    }
+
+    private static void arrayToList(int[] arr) {
+        //Method 1
+        List<int []> list = java.util.Arrays.asList(arr);
+        String arrlistAsString = java.util.Arrays.toString(list.get(0));
+
+
+        // Method 2 : IMP
+        // when arr[] --> List<>
+        List<Integer> listArr1 = Arrays.stream(arr).boxed().toList();
+        System.out.println("output1"+ listArr1);
+
+
+        //Method 3
+        List<Integer> listInt = Arrays.asList(1,2,3);
+        List<String> listString = Arrays.asList("A", "B");
+
+        //Method 4
+        List<Integer> listArr = Arrays.stream(arr)
+                .mapToObj(Integer::valueOf)
+                .collect(Collectors.toList());
+        //System.out.println("listArr"+ listArr);
+
+
+    }
+
+}
+
